Horsey-/Simply-Love-SM5-1

Show "_shared background normal" on ScreenSelectMusic
Previously, only a black background was shown on ScreenSelectMusic. Your choice of background now shows in the background like in "Rainbow Mode" but in a far less blinding fashion.

BGAnimations/ScreenSelectMusic background.lua
@@ -1,4 +1,4 @@
-if not ThemePrefs.Get("RainbowMode") and ThemePrefs.Get("VisualStyle") ~= "SRPG5" then return Def.Actor{ InitCommand=function(self) self:visible(false) end } end
+if not ThemePrefs.Get("RainbowMode") and ThemePrefs.Get("VisualStyle") ~= "SRPG5" then return Def.ActorFrame{ LoadActor(THEME:GetPathB("","_shared background")) } end
 
 return Def.ActorFrame{
 	Def.Quad{

Horsey-/Horseys-Simply-Love

BGAnimations/ScreenSelectMusic background.lua
@@ -1,4 +1,4 @@
-if not ThemePrefs.Get("RainbowMode") and ThemePrefs.Get("VisualStyle") ~= "SRPG6" then return Def.Actor{ InitCommand=function(self) self:visible(false) end } end
+if not ThemePrefs.Get("RainbowMode") and ThemePrefs.Get("VisualStyle") ~= "SRPG6" then return Def.ActorFrame{ LoadActor(THEME:GetPathB("","_shared background")) } end
 
 return Def.ActorFrame{
 	Def.Quad{